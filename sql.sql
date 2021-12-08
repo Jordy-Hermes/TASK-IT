c98c109eeefb7992c1ab92e7e9172963c389be25
Voxdey <59053420+Voxdey@users.noreply.github.com>
2021-12-08 22:28:09 +0100

    Creation bdd

---
+++ sql.sql
@@ -0,0 +1,50 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE DATABASE IF NOT EXISTS `taskit` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `taskit`;
+
+DROP TABLE IF EXISTS `colonne`;
+CREATE TABLE IF NOT EXISTS `colonne` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `nombremax` int(11) NOT NULL,
+  `idproject` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_foreign_idproject` (`idproject`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE IF NOT EXISTS `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `idcolonne` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_foreign_idcolonne` (`idcolonne`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `project`;
+CREATE TABLE IF NOT EXISTS `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_foreign_iduser` (`iduser`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `entreprise` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(512) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
