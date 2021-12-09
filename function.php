df0fca59d3b080c99311bab75fcc621502ad1d38
kingsley <kingsley.potts@ecole-hexagone.com>
2021-12-09 11:39:29 +0100

    test ajout user!

--- function.php
+++ function.php
@@ -13,47 +13,24 @@ function isValid($string){
 	return isset($string) && !empty($string);}
 
 
-if (isset($_POST['forminscription'])) {
+if (isset($_POST['submit'])) {
     
     //attribution données du formulaire à une variable sécurisée
-    $nom = htmlspecialchars($_POST['nom_user']);
-    $prenom = htmlspecialchars($_POST['prenom_user']);
-    $pseudo = htmlspecialchars($_POST['pseudo_user']);
-    $email = htmlspecialchars($_POST['email_user']);
-    $email2 = htmlspecialchars($_POST['sndemail_user']);
-    $password = sha1($_POST['password_user']);
-    $password2 = sha1($_POST['sndpassword_user']);
+    $nom = htmlspecialchars($_POST['firstname']);
+    $prenom = htmlspecialchars($_POST['lastname']);
+    $entreprise = htmlspecialchars($_POST['entreprise']);
+    $email = htmlspecialchars($_POST['email']);
+    $password = sha1($_POST['password']);
 
     // Test avec la fonction isValid si les champs du formulaire ne sont pas vide
-    if (isValid($_POST['nom_user']) &&  isValid($_POST['prenom_user']) && isValid($_POST['email_user']) && isValid($_POST['password_user']) && isValid($_POST['sndpassword_user'])) {
+    if (isValid($_POST['firstname']) &&  isValid($_POST['lastname']) && isValid($_POST['email']) && isValid($_POST['password']) && isValid($_POST['entreprise'])) {
        
+		$insertuser = $db->prepare("INSERT INTO user (firstname, lastname, entreprise, email, password) VALUES (?, ?, ?, ?, ?)");
+		$insertuser->execute(array($nom, $prenom, $entreprise, $email, $password));
 
-        $pseudolenght = strlen($pseudo); // calcul de la taile du pseudo
+	}
+	
 
-        if ($pseudolenght <= 255) { // si le pseudo fait moins de 255 caractères, on réalise une série de conditions
-
-            if ($email == $email2) { // si champ email 1 = champ email 2
-                
-                    if ($password == $password2) { // si champ password1 = champ password 2
-
-                        //on insère dans la base de données les données fournis dans le formualaire
-                        $insertuser = $db->prepare("INSERT INTO user (firstname, lastname, entreprise, email, password) VALUES (?, ?, ?, ?, ?)");
-                        $insertuser->execute(array($nom, $prenom, $pseudo, $email, $password));
-                        $erreurInscription = "Votre compte a bien été créer !<a href=\"connexion.php\">Me connecter</a>";
-
-                    // si il y a des erreurs, on affiche des messages d'erreurs       
-                    } else {
-                        $erreurInscription = "Vos mots de passes ne correspondent pas";
-                    }
-                } else {
-                    $erreurInscription = "Vos adresses mails ne correspondent pas";
-                }
-            } else {
-                $erreurInscription = "Votre pseudo ne doit pas dépasser 255 caractères";
-            }
-        } else {
-            $erreurInscription = "Tout les champs doivent être complétés";
-        }
 }
 
 ?>
