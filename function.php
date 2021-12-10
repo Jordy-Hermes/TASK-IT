4dd4109eedeaa8fa3c796b5a7db1151c5cb651d1
Quentin Jacquot <quentinjacquot@MacBook-Pro-de-Quentin.local>
2021-12-10 10:42:30 +0100

    commit a la con

--- function.php
+++ function.php
@@ -1,33 +1,11 @@
 
 <?php
-try
-{
-$db = new PDO('mysql:host=localhost;dbname=taskit;charset=utf8', 'root', '');
-
-}
-catch (Exception $e)
-{
-die('Erreur : ' . $e->getMessage());
-}
+require_once ("lib/ConnectBDD.php")
 
 function isValid($string){
-	return isset($string) && !empty($string);}
-
-
-    //attribution données du formulaire à une variable sécurisée
-    $nom = htmlspecialchars($_POST['firstname']);
-    $prenom = htmlspecialchars($_POST['lastname']);
-    $entreprise = htmlspecialchars($_POST['entreprise']);
-    $email = htmlspecialchars($_POST['email']);
-    $password = sha1($_POST['password']);
-
-    // Test avec la fonction isValid si les champs du formulaire ne sont pas vide
-    if (isValid($_POST['firstname']) &&  isValid($_POST['lastname']) && isValid($_POST['email']) && isValid($_POST['password']) && isValid($_POST['entreprise'])) {
-       
-		$insertuser = $db->prepare("INSERT INTO user (firstname, lastname, entreprise, email, password) VALUES (?, ?, ?, ?, ?)");
-		$insertuser->execute(array($nom, $prenom, $entreprise, $email, $password));
-
 	
+  return isset($string) && !empty($string);}
+
 }
 
 ?>
---
+++ lib/class.php
@@ -0,0 +1,14 @@
+<?php  
+require('function.php');
+
+class Colonne {
+
+    public function __construct($name, $numberMax) {
+        $this->name = $first_name;
+        $this->numberMax = $numberMax;
+    }
+
+    public function createColumn() {
+            
+    }
+}
