9a4adfae2ffa2914a2da0ccced97b7db4d295ebf
kingsley <kingsley.potts@ecole-hexagone.com>
2021-12-10 14:01:34 +0100

    fusion fonction 1 2

--- function.php
+++ function.php
@@ -3,7 +3,32 @@
 
 function isValid($string){
 	return isset($string) && !empty($string);}
+	
+function addUser() {
 
+    $nom = htmlspecialchars($_POST['firstname']);
+    $prenom = htmlspecialchars($_POST['lastname']);
+    $entreprise = htmlspecialchars($_POST['entreprise']);
+    $email = htmlspecialchars($_POST['email']);
+    $password = sha1($_POST['password']);
+
+    
+    if (isValid($_POST['firstname']) &&  isValid($_POST['lastname']) && isValid($_POST['email']) && isValid($_POST['password']) && isValid($_POST['entreprise'])) {
+
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $reqEmail = $db->prepare("SELECT * FROM user");
+            $reqEmail->execute(array($email));
+            $mailExist = $reqEmail->rowCount();
+
+            if ($mailExist == 0) {
+                $insertuser = $db->prepare("INSERT INTO user (firstname, lastname, entreprise, email, password) VALUES (?, ?, ?, ?, ?)");
+                $insertuser->execute(array($nom, $prenom, $entreprise, $email, $password));
+            }
+            else {
+                echo ("Cet e-mail existe déjà.")
+            }
+        }
+    }
 function connexion($email, $password)
 {
 	$hpassword = sha1($password);
