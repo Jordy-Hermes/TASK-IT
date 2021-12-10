a98f5ab84d229d9f2c0f5ad7029ab26348d010b2
Quentin Jacquot <quentinjacquot@MacBook-Pro-de-Quentin.local>
2021-12-10 14:18:21 +0100

    function inscription user

--- inscription.php
+++ inscription.php
@@ -3,6 +3,14 @@ require_once("function2.php");
 
 if (isset($_POST["submitInscription"])){
 
-    addUser();
+    $nom = htmlspecialchars($_POST['firstname']);
+    $prenom = htmlspecialchars($_POST['lastname']);
+    $entreprise = htmlspecialchars($_POST['entreprise']);
+    $email = htmlspecialchars($_POST['email']);
+    $password = sha1($_POST['password']);
+
+    if (isValid($_POST['firstname']) &&  isValid($_POST['lastname']) && isValid($_POST['email']) && isValid($_POST['password']) && isValid($_POST['entreprise'])) {
     
+        addUser($nom, $prenom, $entreprise, $email, $password);
+    }
 }
