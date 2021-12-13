dfd49ab7f0d56e4fb3340296ddd5df9734e3eaca
JACQUOT <quentin.jacquot@atos.net>
2021-12-13 10:58:01 +0100

    menage

--- function.php
+++ function.php
@@ -1,72 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-	
-function isValid($string){
-	return isset($string) && !empty($string);}
-
-function connectDb(){
-	$username = "root";
-	$password = "";
-	$db = new PDO("mysql:host=localhost;dbname=taskit", $username, $password);
-	return $db;
-}
-	
-function addUser($nom, $prenom, $entreprise, $email, $password) {
-
-		$db = connectDb();
-		
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $reqEmail = $db->prepare("SELECT * FROM user");
-            $reqEmail->execute(array($email));
-            $mailExist = $reqEmail->rowCount();
-
-            if ($mailExist == 0) {
-                $insertuser = $db->prepare("INSERT INTO user (firstname, lastname, entreprise, email, password) VALUES (?, ?, ?, ?, ?)");
-                $insertuser->execute(array($nom, $prenom, $entreprise, $email, $password));
-            }
-            else {
-                echo ("Cet e-mail existe déjà.");
-            }
-        }
-    }
-
-function connexion($email, $password)
-{
-	
-	$hpassword = sha1($password);
-	$isConnected=false;
-	$db = connectDb();
-	$result = $db->prepare("SELECT * FROM user WHERE email=?");
-	$result->execute(array($email));
-	$rows = $result->rowCount();
-
-	if ($rows == 1)
-		{
-			$row= $result->fetch();
-			if($row['password']==$hpassword)
-			{
-				$isConnected=True;
-				echo ("OK");
-				return $isConnected;
-				
-			}
-			else
-			{
-				echo("Mot de passe incorrect");
-				return $isConnected;
-			}
-		}
-	else
-		{
-			echo ("Le mail n'existe pas");
-			return $isConnected;
-		}
-
-}
-
-
-=======
 <?php
 
 function isValid($string){
@@ -158,5 +89,4 @@ function connexion($email, $password)
 }
 
 
->>>>>>> fonctionnel
 ?>
