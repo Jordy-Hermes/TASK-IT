c782c68208ac9bcad39138164f802c9ac688c1c3
Jordy-Hermes <56640527+Jordy-Hermes@users.noreply.github.com>
2021-12-15 02:49:23 -0800

    Add files via upload

--- formConnexion.php
+++ formConnexion.php
@@ -7,36 +7,88 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
-    <link rel="stylesheet" href="styleconnexion.css">
+    <title>Task-It | Connexion</title>
+
+  <!-- Favicons -->
+  <link href="assets/img/logotaskit.png" rel="icon">
+  <link href="assets/img/logotaskit.png" rel="task-it icon">
+
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/styleconnexion.css">
 </head>
 <body>
-    <div class="boxform">
-        <img class="logotaskit" src="img/logotaskit.png"></img>
-        <div class="dispoform">
-            <div class="annonceinscription">
-                <div class="titre">
-                Connexion
-                </div>
-                <div class="description">
-                Organisez vous équipes et votre façon de travailler en Kanban
-                </div>
+
+    <div class="main">
+
+
+    <div class="left">
+        
+        <form name="formconnexion" action="./connexion.php" method="post">
+            <a class="backward" href="./index.html">Accueil</a>
+            <div class="logo"></div>
+            <div class="text-container">
+                <h1>Connexion</h1>
+                <p>Organisez vos equipes et votre facon de travaillez en kanban </p>
+                <div class="google"><a  href="http://"> <div class="img"></div> <p>Continuez avec Google</p></a></div>
+                
+                <span>------------- ou utilisez votre Email -------------</span>
             </div>
-            <form name="formconnexion" action="./connexion.php" method="post">
-                <label for="email">Votre email</label>
-                <div class="saisietxt">
-                    <input type="text" id="email" name="email" placeholder="Votre e-mail">
-                </div> 
-                <label for="pass">Votre mot de passe</label>
-                <div class="saisietxt">
-                    <input type="password" id="pass" placeholder="Votre mot de passe" name="password"> 
-                </div>
-                <div>
-                    <input type="submit" class="bouton" name="connexion"></input>
-                </div>
-            </form>
-        </div>
+            
+            
+            <div class="saisitxt" id="bof">
+            <label for="email">Email</label> 
+            <input type="email"  name="email" id="email"  placeholder="mail@abc.com" required>
+            </div>
+            <div class="saisitxt">  
+            <label for="password">Mot de passe </label> 
+            <input id="password" type="password" name="password" id="password"  placeholder="******"  required>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Mot de passe oublié?</a>
+            </div>
+            
+            
+            <div class="button">
+                <input type="submit" class="mybutton" name="connexion" value="Se connecter">
+            </div>
+
+
+            <div class="form-footer">
+                <p>Pas de compte?</p>
+                <a href="./formInscription.php">Créer nouveau compte</a>
+            </div>
+
+        </form>
+
+
+        
+
+
     </div>
-    <img class="imagetaskit" src="img/imagetaskit.png"></img>
+
+
+    <div class="right">
+
+        
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
 </body>
 </html>
+
--- formInscription.php
+++ formInscription.php
@@ -4,53 +4,119 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inscription</title>
-    <link rel="stylesheet" href="styleinscription.css">
+   
+    <title>Task-It | Inscription</title>
+
+    <!-- Favicons -->
+    <link href="assets/img/logotaskit.png" rel="icon">
+    <link href="assets/img/logotaskit.png" rel="task-it icon">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/styleinscription.css">
 </head>
 <body>
-    <div class="boxform">
-        <img class="logotaskit" src="img/logotaskit.png"></img>
-        <div class="dispoform">
-            <div class="annonceinscription">
-                <div class="titre">
-                Inscription
-                </div>
-                <div class="description">
-                Inscrivez vous facilement et venez partager l’expérience Task-IT
-                </div>
+
+    <div class="main">
+
+  <!-------------------- Case de gauche --------------------------------->
+
+    <div class="left">
+        <form name="forminscription" action="./inscription.php" method="post">
+            <a class="backward" href="./index.html">Accueil</a>
+            <div class="logo"></div>
+            <div class="text-container">
+                <h1>Inscription</h1>
+                <p>Inscrivez vous facilement et venez partager l’expérience Task-IT </p>
             </div>
-            <form name="forminscription" action="./inscription.php" method="post">
-                <div class="saisietxt">
-                    <label for="lastname">Nom</label>
-                    <input type="text" name="lastname" id="lastname">
-                </div>
-                <div class="saisietxt">
-                    <label for="firstname">Prénom</label>
-                    <input type="text" name="firstname" id="firstname">
-                </div>
-                <div class="saisietxt">
-                    <label for="Company">Votre Compagnie</label>
-                    <input type="text" name="entreprise" id="entreprise">
-                </div>
-                <label for="email">Votre email</label>
-                <div>
-                <input type="email" id="email" name="email" required>
-                </div>
-                <div class="saisietxt">
-                    <label for="Password">Votre Mot de passe</label>
-                    <input type="password" name="password" id="password">
-                </div>
-                <div class="saisietxt">
-                    <label for="Password">Confirmer votre mot de passe</label>
-                    <input type="password" name="Password" id="Password">
-                </div>
-                <div>
-                    <input type="submit" class="bouton" name="submitInscription" value="S'inscrire">
-                    </input>
-                </div>
-            </form>
-        </div>
+            
+            <div class="saisitxt">
+                <label for="lastname">Nom</label>
+                <input type="text" name="lastname" id="lastname" placeholder="Nom" required>
+            </div>
+            <div class="saisitxt">
+                <label for="firstname">Prenom</label>
+                <input type="text" name="firstname" id="firstname" placeholder="Prenom" required>
+            </div>
+            <div class="saisitxt">
+                <label for="entreprise">Votre compagnie</label>
+                <input type="text" name="entreprise" id="entreprise" placeholder="Nom de votre compagnie" required>
+            </div>
+            <div class="saisitxt">
+            <label for="email">Email</label> 
+            <input type="email" name="email" id="email" placeholder="Email" required>
+            </div>
+            <div class="saisitxt">  
+            <label for="password">Mot de passe </label> 
+            <input id="password" name="password" type="password" placeholder="******"  required>
+            </div>
+            <div class="saisitxt">
+            <label for="confirm_password">Confirmer le mot de passe </label> 
+            <input id="confirm_password" name="password" type="password" placeholder="******"  required>
+            </div>
+            
+            <div class="button">
+                <input type="submit" class="mybutton" name="submitInscription" value="S'inscrire">
+            </div>
+        </form>
+
+
     </div>
-    <img class="imagetaskit" src="img/imagetaskit.png"></img>
+
+<!---------------------------- Fin de la case de gauche ------------------------------------------------>
+
+
+
+<!---------------------------- Case de droite ------------------------------------------------>
+
+    <div class="right">
+
+        
+    </div>
+<!---------------------------- Fin de la case de droite ------------------------------------------------>
+
+
+
+
+
+
+
+<!---------------------------- Script pour s'assurer que le mot de passe concorde avec celui taper dans "confirmer mot de passe" ------------------------------------------------>
+
+<script>
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+      
+    confirm_password.setCustomValidity("Les mots de passe saisis ne concordent pas.");
+    
+    confirm_password.classList.add("nomatch");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
+
+</script>
+
+<!---------------------------- Fin du script pour  ------------------------------------------------>
+
+
+
+
+
+
+
+</div>
+
+
+
+
 </body>
 </html>
+
